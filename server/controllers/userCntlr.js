315c5df0445c42f557a3cb0b11846713bc0cb36f
peterwengg <peterwengjy@gmail.com>
2023-12-13 10:46:54 +0800

    Added register user end point
    Added register user end point

--- server/controllers/userCntlr.js
+++ server/controllers/userCntlr.js
@@ -1,9 +1,19 @@
-import asyncHandler from 'express-async-handler'
-import {prisma} from "../config/prismaConfig.js"
+import asyncHandler from "express-async-handler";
+import { prisma } from "../config/prismaConfig.js";
 
-export const createUser = asyncHandler(async(req, res) => {
-    console.log("Creating a User");
-    let {email} = req.body;
+export const createUser = asyncHandler(async (req, res) => {
+console.log("Creating a User");
+let { email } = req.body;
 
-    console.log(email);
-})
+// Check if user exists before creating
+const userExists = await prisma.user.findUnique({ where: { email } });
+
+if (!userExists) {
+    const user = await prisma.user.create({ data: req.body });
+    res.send({
+    message: "User registered successfully",
+    user: user,
+    });
+}
+else res.send(201).json({ message: "User already registered" });
+});
