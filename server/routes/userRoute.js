d22cc54d8e99561badbba4408990f4d87d8ff755
peterwengg <peterwengjy@gmail.com>
2023-12-13 10:47:13 +0800

    Added register user routes and controller
    Added register user routes and controller

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import {createUser} from '../controllers/userCntlr.js';
+import { createUser } from '../controllers/userCntlr.js';
 const router = express.Router();
 
-router.post("register", createUser)
+router.post("/register", createUser)
 
-export {router as userRouteya}
+export {router as userRoute}
